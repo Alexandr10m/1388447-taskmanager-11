197d84cf56ee81dcfee4b497c2b9b50114496875
Alexandr10m <serbackij@gmail.com>
2020-04-09 19:10:02 +0300

    Добавил в utils форматирование даты и времени

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,14 @@ const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
-export {render};
+const castTimeFormat = (value) => {
+  return value < 10 ? `0${value}` : String(value);
+};
+
+const formatTime = (date) => {
+  const hours = castTimeFormat(date.getHours() % 12);
+  const minutes = castTimeFormat(date.getMinutes());
+
+  return `${hours}: ${minutes}`;
+};
+export {render, formatTime};
