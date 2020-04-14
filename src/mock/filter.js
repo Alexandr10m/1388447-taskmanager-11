b6a103ebe9821882b6eb4cfbcb6986d286d61845
Alexandr10m <serbackij@gmail.com>
2020-04-14 14:33:43 +0300

    Отредактировал файл src/mock/filter.js

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -1,9 +1,9 @@
-const filterName = [
+const FILTER_NAME = [
   `all`, `overdue`, `today`, `favorites`, `repeating`, `archive`
 ];
 
 const generateFilters = () => {
-  return filterName.map((it) => {
+  return FILTER_NAME.map((it) => {
     return {
       name: it,
       count: Math.floor(Math.random() * 22)
