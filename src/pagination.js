d7365e9e3912fec6365b95676dd51fc1288576ad
Alexandr10m <serbackij@gmail.com>
2020-04-17 14:10:39 +0300

    Отредактировал пагинацию

--- src/pagination.js
+++ src/pagination.js
@@ -1,23 +1,21 @@
-import {render} from './utils';
-import {createTaskTemplate} from './components/Task';
-
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
 
 
-const pagination = (btn, array) => {
-  btn.addEventListener(`click`, () => {
+const pagination = (btnComponent, array, fn) => {
+  btnComponent.getElement().addEventListener(`click`, () => {
     const taskListElement = document.querySelector(`.board__tasks`);
     const prevTasksCount = showingTasksCount;
 
     showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
     array.slice(prevTasksCount, showingTasksCount)
-      .forEach((task) => render(taskListElement, createTaskTemplate(task)));
+      .forEach((task) => fn(taskListElement, task));
 
     if (showingTasksCount >= array.length) {
-      btn.remove();
+      btnComponent.getElement().remove();
+      btnComponent.removeElement();
     }
   });
 };
