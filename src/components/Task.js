811b472d70fe56bae51189339c56fbbdc2765ff8
Alexandr10m <serbackij@gmail.com>
2020-04-23 18:42:20 +0300

    Добавил абстракный компонент

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,6 @@
 import {MONTH_NAMES} from "../constants";
-import {formatTime, createElement} from "../utils";
+import {formatTime} from "../utils";
+import AbstractComponent from "./abstract-component.js";
 
 const createTaskTemplate = ({description, dueDate, color, repeatingDays, isArchive, isFavorite}) => {
   const isDateShowing = !!dueDate;
@@ -63,24 +64,13 @@ const createTaskTemplate = ({description, dueDate, color, repeatingDays, isArchi
 };
 
 
-export default class Task {
+export default class Task extends AbstractComponent {
   constructor(task) {
+    super();
     this._task = task;
-    this._element = null;
   }
 
   getTemplate() {
     return createTaskTemplate(this._task);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
