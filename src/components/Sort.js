daf33e5a63bc0bf035d4976ed38e562bde84b7cb
Alexandr10m <serbackij@gmail.com>
2020-04-27 13:48:02 +0300

    Отредактировал шаблон сортировки

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,16 +1,27 @@
 import AbstractComponent from "./abstract-component.js";
+import {SORT_OPTIONS} from "../constants";
+
+
 const SortType = {
   DATE_DOWN: `date-down`,
   DATE_UP: `date-up`,
   DEFAULT: `default`
 };
 
+const createSortOption = (name) => {
+  return `<a href="#" data-sort-type="${SortType[name]}" class="board__filter">SORT BY ${SORT_OPTIONS[name]}</a>`;
+};
+
 const createSortTemplate = () => {
+  let sortOptionMarkup = ``;
+
+  for (const [key] of Object.entries(SORT_OPTIONS)) {
+    sortOptionMarkup += createSortOption(key);
+  }
+
   return (
     `<div class="board__filter-list">
-      <a href="#" data-sort-type="${SortType.DEFAULT}" class="board__filter">SORT BY DEFAULT</a>
-      <a href="#" data-sort-type="${SortType.DATE_UP}" class="board__filter">SORT BY DATE up</a>
-      <a href="#" data-sort-type="${SortType.DATE_DOWN}" class="board__filter">SORT BY DATE down</a>
+    ${sortOptionMarkup}
     </div>`
   );
 };
