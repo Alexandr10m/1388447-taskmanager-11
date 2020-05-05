8fcac9eea4c55f662cdd34fe5a7edaf6c5919e8e
Alexandr10m <serbackij@gmail.com>
2020-05-05 19:27:28 +0300

    Добавил функцию formatDate и адаптировал formatTime под библиотеку  moment

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,12 +1,20 @@
-const castTimeFormat = (value) => {
-  return value < 10 ? `0${value}` : String(value);
-};
+import moment from "moment";
+
+
+// const castTimeFormat = (value) => {
+//   return value < 10 ? `0${value}` : String(value);
+// };
 
 const formatTime = (date) => {
-  const hours = castTimeFormat(date.getHours() % 12);
-  const minutes = castTimeFormat(date.getMinutes());
+  // const hours = castTimeFormat(date.getHours() % 12);
+  // const minutes = castTimeFormat(date.getMinutes());
 
-  return `${hours}: ${minutes}`;
+  // return `${hours}: ${minutes}`;
+  return moment(date).format(`hh:mm`);
 };
 
-export {formatTime};
+const formatDate = (date) => {
+  return moment(date).format(`DD MMM`);
+};
+
+export {formatTime, formatDate};
